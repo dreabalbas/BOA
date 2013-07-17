cfa0ae2d08c9013e78c9e377d23bbf23c2ed9b2f
dreabalbas <drea.balbas@gmail.com>
2013-07-16 20:20:37 -0430

    habia faltado la contrasena en la bd

--- BOA/BOA01.sql
+++ BOA/BOA01.sql
@@ -10,7 +10,8 @@ CREATE TABLE usuario (
   nombreusuario     TEXT PRIMARY KEY UNIQUE,
   nombres           TEXT,
   apellidos         TEXT,
-  email             TEXT
+  email             TEXT,
+  contrasena        TEXT
 );
 
 CREATE TABLE boa (
@@ -27,9 +28,9 @@ CREATE TABLE amigo (
 );
 
 --INSERCIONES POR DEFAULT
-INSERT INTO usuario VALUES ('origds', 'Oriana', 'Gomez', 'origds@gmail.com');
-INSERT INTO usuario VALUES ('betocolsf', 'Alberto', 'Cols', 'betocolsf@gmail.com');
-INSERT INTO usuario VALUES ('dreabalbas', 'Andrea Carolina', 'Balbas Quintero', 'drea.balbas@gmail.com');
+INSERT INTO usuario VALUES ('origds', 'Oriana', 'Gomez', 'origds@gmail.com', 'oriana');
+INSERT INTO usuario VALUES ('betocolsf', 'Alberto', 'Cols', 'betocolsf@gmail.com', 'alberto');
+INSERT INTO usuario VALUES ('dreabalbas', 'Andrea Carolina', 'Balbas Quintero', 'drea.balbas@gmail.com', 'andrea');
 INSERT INTO boa VALUES ('origds', 'boa 1 de oriana', '2011-05-10 21:02:34');
 INSERT INTO boa VALUES ('origds', 'boa 2 de oriana', '2011-05-10 21:02:34');
 INSERT INTO boa VALUES ('betocolsf', 'boa 1 de beto', '2011-05-10 21:02:34');
--- BOA/lib/BOA/Form/Usuario.pm
+++ BOA/lib/BOA/Form/Usuario.pm
@@ -5,12 +5,13 @@ extends 'HTML::FormHandler::Model::DBIC';
 use namespace::autoclean;
 
 has '+item_class' => ( default =>'Usuarios' );
-has_field 'nombreusuario' => ( type => 'Text', apply => [{check  => qr/^[0-9a-z]*\z/,
+has_field 'nombreusuario' => ( type => 'Text', required => 1, maxlength => 20, 
+				apply => [{check  => qr/^[0-9a-z]*\z/,
 				message => 'Contains invalid characters'}] );
-has_field 'nombres' => ( type => 'Text' );
-has_field 'apellidos' => ( type => 'Text');
-has_field 'email' => ( type => 'Email');
-has_field 'submit' => ( type => 'Submit', value => 'Submit' );
+has_field 'nombres' => ( type => 'Text', required => 1, maxlength => 60 );
+has_field 'apellidos' => ( type => 'Text', required => 1, maxlength => 60);
+has_field 'email' => ( type => 'Email', required => 1);
+has_field 'submit' => ( type => 'Submit', value => 'Guardar' );
 
 __PACKAGE__->meta->make_immutable;
 1;
